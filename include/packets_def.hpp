54f9a0802653119377f8998bb9417082ddf4c1a5
Maximus Seniorem <9930974+MaximusSeniorem@users.noreply.github.com>
2023-04-04 13:53:01 +0200

    initial commit

---
+++ include/packets_def.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <cstdint>
+
+
+template <class D>
+class PacketBase{
+    public:
+};
---
+++ include/vars_def.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <climits> 
+#include <concepts>
+#include <cstdint>
+#include "vars_enum.hpp"
+
+template <std::unsigned_integral T, size_t N>
+    requires (N> 0 && N <= (sizeof(T)*CHAR_BIT))
+struct Var{
+    T x : N;
+    size_t len = N;
+};
