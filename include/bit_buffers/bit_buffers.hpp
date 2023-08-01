eb1f3122e93328fe81f2388a4e41be76f9e61b40
Maximus Seniorem <9930974+MaximusSeniorem@users.noreply.github.com>
2023-08-01 18:42:53 +0200

    small renaming

--- include/bit_buffers/bit_buffers.hpp
+++ include/bit_buffers/bit_buffers.hpp
@@ -14,8 +14,12 @@ namespace bb{
         
         virtual ~bitbuffer() = default;
 
-        std::size_t bit_size(){ 
+        constexpr std::size_t bit_size() { 
             return (m_offset%CHAR_BIT == 0) ? m_offset/CHAR_BIT : m_offset/CHAR_BIT + 1;
+        }  
+        
+        constexpr std::size_t bit_capacity() { 
+            return m_bsize;
         }
 
     protected:
@@ -32,35 +36,37 @@ namespace bb{
 
         template <typename T>
         ibitstream& read(T & val, std::size_t val_len){
-            std::size_t index = this->m_offset / CHAR_BIT;
-            const std::size_t val_size = sizeof(T);
-            std::uint8_t t_len = val_size * CHAR_BIT;
-            auto val_ptr = std::as_writable_bytes(std::span{&val, 1});
-            
             //oob
             if(this->m_offset + val_len >= this->m_bsize){
                 //hande buffer read oob error, throws(--) or internal error at buffer level
                 return *this;
             }
 
-            for (auto i = 0; i < val_size; ++i){
-                val_ptr[i] = this->m_buf[i + index];
+            const std::size_t buf_idx = this->m_offset / CHAR_BIT;
+            std::size_t buf_shift = this->m_offset % CHAR_BIT;
+
+            const std::size_t t_sz = sizeof(T);
+            const std::uint8_t t_len = t_sz * CHAR_BIT;
+            auto val_ptr = std::as_writable_bytes(std::span{&val, 1});
+        
+            for (auto i = 0; i < t_sz; ++i){
+                val_ptr[i] = this->m_buf[i + buf_idx];
             }
 
-            std::size_t val_last_index = val_size - 1;
+            std::size_t val_last_index = t_sz - 1;
             if constexpr (std::endian::native == std::endian::little){
                 val = std::byteswap(val);
                 val_last_index = 0;
             }
-             
-            std::uint8_t loc_shift = this->m_offset % CHAR_BIT;
-            val <<= loc_shift;
+
+
+            val <<= buf_shift;
             val >>= (t_len - val_len);
 
             //check if there's bits left to read
-            loc_shift = loc_shift + val_len - t_len;
-            if(loc_shift > 0)
-                val_ptr[val_last_index] |= this->m_buf[val_size + index] >> (CHAR_BIT - loc_shift);
+            buf_shift = buf_shift + val_len - t_len;
+            if(buf_shift > 0)
+                val_ptr[val_last_index] |= this->m_buf[t_sz + buf_idx] >> (CHAR_BIT - buf_shift);
             
             this->m_offset += val_len;
             return *this;
@@ -75,39 +81,46 @@ namespace bb{
 
         template <typename T>
         obitstream& write(T val, std::size_t val_len){
-            const std::size_t index = this->m_offset / CHAR_BIT;
-            std::uint8_t loc_shift = this->m_offset % CHAR_BIT;
-            const std::size_t val_size = sizeof(T);
-            const std::uint8_t t_len = val_size * CHAR_BIT;
-            T val_shifted = val << (t_len - val_len);
-            auto val_ptr = std::as_bytes(std::span{&val_shifted, 1});
-
             //buffer overflow
             if(this->m_offset + val_len >= this->m_bsize){
                 //hande buffer overflow error, throws(--) or internal error at buffer level
                 return *this;
             }
 
-            val_shifted >>= loc_shift;
-            std::size_t val_last_index = val_size - 1;
+            const std::size_t buf_idx = this->m_offset / CHAR_BIT;
+            std::size_t buf_shift = this->m_offset % CHAR_BIT;
+
+            const std::size_t t_sz = sizeof(T);
+            const std::size_t t_len = t_sz * CHAR_BIT;
+
+            T val_shifted = val << (t_len - val_len);
+            auto val_ptr = std::as_bytes(std::span{&val_shifted, 1});
+
+            /* apply buffer shift */
+            val_shifted >>= buf_shift;
+
+            /* handle endianess: buffer is big-endian */
+            std::size_t last_index = t_sz - 1;
             if constexpr (std::endian::native == std::endian::little){
                 val_shifted = std::byteswap(val_shifted);
-                val_last_index = 0;
+                last_index = 0;
             }
-            for (auto i = 0; i < val_size; ++i){
-                this->m_buf[index + i] |= val_ptr[i];
+
+            /* copy on buffer */
+            for (auto i = 0; i < t_sz; ++i){
+                this->m_buf[buf_idx + i] |= val_ptr[i];
             }
 
             //check if there's bits left to write
-            loc_shift = loc_shift + val_len - t_len;
-            if (loc_shift > 0){
-                val_shifted = val << CHAR_BIT - loc_shift;
-                this->m_buf[val_size + index] |= val_ptr[val_last_index];
+            buf_shift = buf_shift + val_len - t_len;
+            if (buf_shift > 0){
+                val_shifted = val << CHAR_BIT - buf_shift;
+                this->m_buf[t_sz + buf_idx] |= val_ptr[last_index];
             }
         
             this->m_offset += val_len;
 
             return *this;
-        }    
+        }
     };
 }
